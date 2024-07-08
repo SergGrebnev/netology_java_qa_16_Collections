90c9b307fbbbec8b0c582bb45495d0c5f3dbcd79
SergGrebnev <serggrebnev79@gmail.com>
2024-07-08 16:23:41 +0700

    Add HashMap

--- src/main/java/ru/netology/game/Game.java
+++ src/main/java/ru/netology/game/Game.java
@@ -2,54 +2,44 @@ package ru.netology.game;
 
 import ru.netology.exception.NotRegisteredException;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 public class Game {
 
-    //Список всех зарегистрированных игроков
-    List<Player> registeredList = new ArrayList<>();
+    //Мапа (словарь) всех зарегистрированных игроков
+    HashMap<String, Player> registeredList = new HashMap<>();
 
     //метод регистрации игрока, если игрок не зарегистрирован,
     // то он не сможет играть в турнире
     public void register(Player player) {
 
-        registeredList.add(player);
+        registeredList.put(player.getName(), player);
 
     }
 
     //метод соревнования между двумя игроками
     public int round(String playerName1, String playerName2) {
-        int numberPlayer1 = -1;
-        int numberPlayer2 = -1;
-        for (int i = 0; i < registeredList.size(); i++) {
-            if (registeredList.get(i).getName() == playerName1) { //определение номера в списке игрока 1
-                numberPlayer1 = i;
+            if (!registeredList.containsKey(playerName1)) { //проверка регистрации игрока 1
+                throw new NotRegisteredException(
+                        "Игрок " + playerName1 + " не зарегистрирован"
+                );
             }
-            if (registeredList.get(i).getName() == playerName2) { //определение номера в списке игрока 2
-                numberPlayer2 = i;
+            if (!registeredList.containsKey(playerName2)) { //проверка регистрации игрока 2
+                throw new NotRegisteredException(
+                        "Игрок " + playerName2 + " не зарегистрирован"
+                );
             }
-        }
-        if (numberPlayer1 < 0) {      //проверка регистрации игрока
-            throw new NotRegisteredException(
-                    "Игрок " + playerName1 + " не зарегистрирован"
-            );
-        }
-        if (numberPlayer2 < 0) {     //проверка регистрации игрока
-            throw new NotRegisteredException(
-                    "Игрок " + playerName2 + " не зарегистрирован"
-            );
-        }
+
 
         // Битва
         //0 - ничья,
         // 1 - 1 игрок победил,
         // 2 - 2 игрок победил
-        if (registeredList.get(numberPlayer1).getStrength() ==
-                registeredList.get(numberPlayer2).getStrength()) {
+        if (registeredList.get(playerName1).getStrength() ==
+                registeredList.get(playerName2).getStrength()) {
             return 0;
-        } else if (registeredList.get(numberPlayer1).getStrength() >
-                registeredList.get(numberPlayer2).getStrength()) {
+        } else if (registeredList.get(playerName1).getStrength() >
+                registeredList.get(playerName2).getStrength()) {
             return 1;
         } else {
             return 2;
