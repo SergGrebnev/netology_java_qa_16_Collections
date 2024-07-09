c7e07b1b52c973d8403e2a97584ecef98fc947d9
SergGrebnev <serggrebnev79@gmail.com>
2024-07-09 17:40:14 +0700

    Add method numberPlayer

--- src/main/java/ru/netology/game/Game.java
+++ src/main/java/ru/netology/game/Game.java
@@ -13,35 +13,31 @@ public class Game {
     //метод регистрации игрока, если игрок не зарегистрирован,
     // то он не сможет играть в турнире
     public void register(Player player) {
-
         registeredList.add(player);
+    }
 
+    //Метод определения номера игрока в списке по его имени
+    //-1 - в списках не значится
+    private int numberPlayer(String playerName) {
+        int number = -1;
+        for (int i = 0; i < registeredList.size(); i++) {
+            if (registeredList.get(i).getName().equals(playerName)) {
+                number = i;
+            }
+        }
+        if (number < 0) {      //проверка регистрации игрока
+            throw new NotRegisteredException(
+                    "Игрок " + playerName + " не зарегистрирован"
+            );
+        }
+        return number;
     }
 
     //метод соревнования между двумя игроками
     public int round(String playerName1, String playerName2) {
-        int numberPlayer1 = -1;
-        int numberPlayer2 = -1;
-        for (int i = 0; i < registeredList.size(); i++) {
-            if (registeredList.get(i).getName() == playerName1) { //определение номера в списке игрока 1
-                numberPlayer1 = i;
-            }
-            if (registeredList.get(i).getName() == playerName2) { //определение номера в списке игрока 2
-                numberPlayer2 = i;
-            }
-        }
-        if (numberPlayer1 < 0) {      //проверка регистрации игрока
-            throw new NotRegisteredException(
-                    "Игрок " + playerName1 + " не зарегистрирован"
-            );
-        }
-        if (numberPlayer2 < 0) {     //проверка регистрации игрока
-            throw new NotRegisteredException(
-                    "Игрок " + playerName2 + " не зарегистрирован"
-            );
-        }
+        int numberPlayer1 = numberPlayer(playerName1);
+        int numberPlayer2 = numberPlayer(playerName2);
 
-        // Битва
         //0 - ничья,
         // 1 - 1 игрок победил,
         // 2 - 2 игрок победил
